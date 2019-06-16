d6f6c6c3703cce0a48bea25ce1507a403e208e0e
Adjoa Ntoso <adjoaan@gmail.com>
2019-06-16 02:31:59 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  cute_dogs = ["wow", "I", "am", "really", "learning", "arrays!"]
+  cute_dogs.sort
 end
 
 def using_reverse(array)
-
+  cute_dogs = ["wow", "I", "am", "really", "learning", "arrays!"]
+  cute_dogs.reverse
 end
 
 def using_first(array)
-
+  cute_dogs = ["wow", "I", "am", "really", "learning", "arrays!"]
+  cute_dogs.first
 end
 
 def using_last(array)
-
+  cute_dogs = ["wow", "I", "am", "really", "learning", "arrays!"]
+  cute_dogs.last
 end
 
 def using_size(array)
-
+  cute_dogs = ["wow", "I", "am", "really", "learning", "arrays!"]
+  cute_dogs.size
 end
